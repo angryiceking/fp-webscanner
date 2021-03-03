9ed375996ea463a238f31ef5f7f3515715d49634
Carl Almayda <carl.almayda@stratodigital.io>
2021-03-03 09:18:37 +0800

    Add matching capability on fp

--- marie/views.py
+++ marie/views.py
@@ -9,11 +9,64 @@ from .forms import UserRegistrationForm
 
 # Create your views here.
 
-class MarieBiscuit(View):
+class MarieGetUser(View):
+    
+    def post(self, request):
+        username = request.POST.get('username')
+        user = User.objects.get(username=username)
+        userprofile = UserProfile.objects.get(user=user.id)
+        template_base64_response = userprofile.fp_template_base64
+        return HttpResponse(template_base64_response, status=200)
+
+
+class MarieRegister(View):
 
     def get(self, request):
         form = UserRegistrationForm()
-        return render(request, 'index.html', {"form": form})
+        return render(request, 'register.html', {"form": form})
+
+    def post(self, request):
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = request.POST.get('username')
+            fp_template_base64 = request.POST.get('fp_template_base64')
+            fp_bmp_base64 = request.POST.get('fp_bmp_base64')
+            fp_encode_wsq = request.POST.get('fp_encode_wsq')
+            fp_image_height = request.POST.get('fp_image_height')
+            fp_image_width = request.POST.get('fp_image_width')
+            fp_image_resolution = request.POST.get('fp_image_resolution')
+            fp_image_quality = request.POST.get('fp_image_quality')
+            fp_image_nfiq = request.POST.get('fp_image_nfiq')
+            fp_image_wsq_size = request.POST.get('fp_image_wsq_size')
+            fp_image_dimension = {
+                "height": fp_image_height,
+                "width": fp_image_width,
+                "resolution": fp_image_resolution,
+                "quality": fp_image_quality
+            }
+            user = User.objects.get(username=username)
+            user_profile = UserProfile(
+                user_id=user.id,
+                fp_template_base64=fp_template_base64,
+                fp_bmp_base64=fp_bmp_base64,
+                fp_encode_wsq=fp_encode_wsq,
+                fp_image_dimension=fp_image_dimension,
+                fp_image_nfiq=fp_image_nfiq,
+                fp_image_wsq_size=fp_image_wsq_size
+            )
+            user_profile.save()
+            print('profile saved', fp_image_dimension, username)
+        else:
+            print('invalid')
+        return HttpResponse('nice, a successfull post request on my index page, hmm', status=200)
+
+
+class MarieLogin(View):
+
+    def get(self, request):
+        form = UserRegistrationForm()
+        return render(request, 'login.html', {"form": form})
 
     def post(self, request):
         form = UserRegistrationForm(request.POST)
